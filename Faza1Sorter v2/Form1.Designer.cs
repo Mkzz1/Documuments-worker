1c22092fb7cbaeef27ef0b40fe84fd70ed6d0c3d
Mkzz <mmikozowy@gmail.com>
2022-02-27 13:39:04 +0100

    v4

--- Faza1Sorter v2/Form1.Designer.cs
+++ Faza1Sorter v2/Form1.Designer.cs
@@ -34,7 +34,11 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.materialSwitch1 = new MaterialSkin.Controls.MaterialSwitch();
+            this.materialTextBox21 = new MaterialSkin.Controls.MaterialTextBox2();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -43,14 +47,14 @@
             // 
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(25, 50);
+            this.panel1.Location = new System.Drawing.Point(25, 127);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(622, 581);
             this.panel1.TabIndex = 0;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(69, 38);
+            this.pictureBox1.Location = new System.Drawing.Point(69, 42);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(500, 500);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -63,7 +67,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(25, 12);
+            this.button1.Location = new System.Drawing.Point(25, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(117, 32);
             this.button1.TabIndex = 2;
@@ -73,7 +77,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(665, 53);
+            this.button2.Location = new System.Drawing.Point(25, 784);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(253, 54);
             this.button2.TabIndex = 3;
@@ -83,9 +87,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(665, 123);
+            this.button3.Location = new System.Drawing.Point(284, 784);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(253, 57);
+            this.button3.Size = new System.Drawing.Size(253, 54);
             this.button3.TabIndex = 4;
             this.button3.Text = "TELEFON / SMS";
             this.button3.UseVisualStyleBackColor = true;
@@ -93,42 +97,111 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(665, 197);
+            this.button4.Location = new System.Drawing.Point(543, 784);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(253, 52);
+            this.button4.Size = new System.Drawing.Size(253, 54);
             this.button4.TabIndex = 5;
             this.button4.Text = "6110";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // richTextBox1
+            // button5
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(484, 12);
-            this.richTextBox1.MaxLength = 31234649;
-            this.richTextBox1.Multiline = false;
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(163, 32);
-            this.richTextBox1.TabIndex = 6;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.button5.Location = new System.Drawing.Point(94, 732);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 30);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Poprzedni";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_navigation);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(467, 732);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 30);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Następny";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_navigation);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(665, 127);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(253, 377);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.listView1_ItemSelectionChanged);
+            // 
+            // materialSwitch1
+            // 
+            this.materialSwitch1.AutoSize = true;
+            this.materialSwitch1.Depth = 0;
+            this.materialSwitch1.Location = new System.Drawing.Point(783, 75);
+            this.materialSwitch1.Margin = new System.Windows.Forms.Padding(0);
+            this.materialSwitch1.MouseLocation = new System.Drawing.Point(-1, -1);
+            this.materialSwitch1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSwitch1.Name = "materialSwitch1";
+            this.materialSwitch1.Ripple = true;
+            this.materialSwitch1.Size = new System.Drawing.Size(135, 37);
+            this.materialSwitch1.TabIndex = 10;
+            this.materialSwitch1.Text = "Dark mode";
+            this.materialSwitch1.UseVisualStyleBackColor = true;
+            this.materialSwitch1.CheckedChanged += new System.EventHandler(this.materialSwitch1_CheckedChanged);
+            // 
+            // materialTextBox21
+            // 
+            this.materialTextBox21.AnimateReadOnly = false;
+            this.materialTextBox21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.materialTextBox21.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.materialTextBox21.Depth = 0;
+            this.materialTextBox21.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBox21.HideSelection = true;
+            this.materialTextBox21.LeadingIcon = null;
+            this.materialTextBox21.Location = new System.Drawing.Point(665, 523);
+            this.materialTextBox21.MaxLength = 32767;
+            this.materialTextBox21.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBox21.Name = "materialTextBox21";
+            this.materialTextBox21.PasswordChar = '\0';
+            this.materialTextBox21.PrefixSuffixText = null;
+            this.materialTextBox21.ReadOnly = false;
+            this.materialTextBox21.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.materialTextBox21.SelectedText = "";
+            this.materialTextBox21.SelectionLength = 0;
+            this.materialTextBox21.SelectionStart = 0;
+            this.materialTextBox21.ShortcutsEnabled = true;
+            this.materialTextBox21.Size = new System.Drawing.Size(250, 48);
+            this.materialTextBox21.TabIndex = 11;
+            this.materialTextBox21.TabStop = false;
+            this.materialTextBox21.Text = "materialTextBox21";
+            this.materialTextBox21.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.materialTextBox21.TrailingIcon = null;
+            this.materialTextBox21.UseSystemPasswordChar = false;
+            this.materialTextBox21.Click += new System.EventHandler(this.materialTextBox21_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(930, 643);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(944, 869);
+            this.Controls.Add(this.materialTextBox21);
+            this.Controls.Add(this.materialSwitch1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sorter Fazy 1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -136,11 +209,14 @@
 
         private Panel panel1;
         private PictureBox pictureBox1;
-        private TrackBar trackBar1;
         private Button button1;
         private Button button2;
         private Button button3;
         private Button button4;
-        private RichTextBox richTextBox1;
+        private Button button5;
+        private Button button6;
+        private ListView listView1;
+        private MaterialSkin.Controls.MaterialSwitch materialSwitch1;
+        private MaterialSkin.Controls.MaterialTextBox2 materialTextBox21;
     }
 }
--- Faza1Sorter v2/Form1.cs
+++ Faza1Sorter v2/Form1.cs
@@ -1,9 +1,14 @@
 using System.IO;
+using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace Faza1Sorter_v2
 {
-    public partial class Form1 : Form
+    public partial class Form1 : MaterialForm
     {
+        private List<Image> LoadedImages { get; set; }
+        private int SelectedImageIndex = 0;
         bool Dragging;
         int xPos;
         int yPos;
@@ -11,6 +16,11 @@ namespace Faza1Sorter_v2
         {
             InitializeComponent();
             this.pictureBox1.MouseWheel += PictureBox1_MouseWheel;
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
         }
 
         private void PictureBox1_MouseWheel(object? sender, MouseEventArgs e)
@@ -27,60 +37,53 @@ namespace Faza1Sorter_v2
             }
         }
 
-        Image ZoomPicture(Image img, Size size)
-        {
-            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width), Convert.ToInt32(img.Height * size.Height));
-            Graphics g = Graphics.FromImage(bm);
-            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Bicubic;
-            return bm;
-        }
-
-        PictureBox org;
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            //trackBar1.Minimum = 1;
-            //trackBar1.Maximum = 6;
-            //trackBar1.SmallChange = 1;
-            //trackBar1.LargeChange = 1;
-            //trackBar1.UseWaitCursor = false;
-
             this.DoubleBuffered = true;
-            //org = new PictureBox();
-            //org.Image = pictureBox1.Image;
         }
-
-        private void trackBar1_Scroll(object sender, EventArgs e)
+        private void LoadImagesFromFolder(string[] paths)
         {
-            if(trackBar1.Value != 0)
-            {
-                pictureBox1.Image = null;
-                pictureBox1.Image = ZoomPicture(org.Image, new Size(trackBar1.Value, trackBar1.Value));
+            LoadedImages = new List<Image>();
+            
 
+            foreach(var path in paths)
+            { 
+                var tempImage = Image.FromFile(path);
+                LoadedImages.Add(tempImage);
             }
-
-
         }
 
         public void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog od = new OpenFileDialog();
-            var onlyFileName = System.IO.Path.GetFileName(od.FileName);
-            if (od.ShowDialog() == DialogResult.OK)
+            FolderBrowserDialog FolderBrowser = new FolderBrowserDialog();
+            if(FolderBrowser.ShowDialog() == DialogResult.OK)
             {
-                org = new PictureBox();
-                org.Load(od.FileName);
-                pictureBox1.Cursor = Cursors.Hand;
-                pictureBox1.Load(od.FileName);
-                richTextBox1.Text = Path.GetFileName(od.FileName);
-                
+                var selectedDirectory = FolderBrowser.SelectedPath;
+                var imagePaths = Directory.GetFiles(selectedDirectory);
+
+                LoadImagesFromFolder(imagePaths);
+
+                //Inicializacja listy plików
+                ImageList images = new ImageList();
+                images.ImageSize = new System.Drawing.Size(130, 130);
+                foreach (var image in LoadedImages)
+                {
+                    images.Images.Add(image);
+                }
+                //ustawianie listview z imagelsit
+                listView1.LargeImageList = images;
+
+                for (int itemIndex = 1; itemIndex < LoadedImages.Count; itemIndex++)
+                {
+                    listView1.Items.Add(new ListViewItem($"{itemIndex}", itemIndex - 1));
+                }
             }
+        }
+        public void pictureBox1_Click(object sender, EventArgs e)
+        {
             
         }
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-           
-        }
+
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e) { Dragging = false; }
         private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
         {
@@ -100,25 +103,133 @@ namespace Faza1Sorter_v2
                 c.Left = e.X + c.Left - xPos;
             }
         }
-
+        //przycisk do BK i tranzyt
         public void button2_Click(object sender, EventArgs e)
         {
             
+            string startFodler = $@"C:\Users\Mkzz\Desktop\doki";
+            string destinyFodler = @"C:\Users\Mkzz\Desktop\doki\Test1\";
+
+            string[] files = Directory.GetFiles(startFodler, "*.tif");
+
+            pictureBox1.Dispose();
+            listView1.Dispose();
+            
+            foreach (string f in files)
+            {
+                string fName = f.Substring(startFodler.Length +1);
+                File.Copy(Path.Combine(startFodler, fName), Path.Combine(destinyFodler, fName), true);
+
+            }
+            foreach (string f in files)
+            {
+                File.Delete(f);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            string startFodler = $@"C:\Users\Mkzz\Desktop\doki";
+            string destinyFodler = @"C:\Users\Mkzz\Desktop\doki\Test2\";
 
+            string[] files = Directory.GetFiles(startFodler, "*.tif");
+
+            foreach (string f in files)
+            {
+                string fName = f.Substring(startFodler.Length + 1);
+                File.Copy(Path.Combine(startFodler, fName), Path.Combine(destinyFodler, fName), true);
+
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            string startFodler = $@"C:\Users\Mkzz\Desktop\doki";
+            string destinyFodler = @"C:\Users\Mkzz\Desktop\doki\Test3\";
 
+            string[] files = Directory.GetFiles(startFodler, "*.tif");
+
+            foreach (string f in files)
+            {
+                string fName = f.Substring(startFodler.Length + 1);
+                File.Copy(Path.Combine(startFodler, fName), Path.Combine(destinyFodler, fName), true);
+
+            }
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private void button_navigation(object sender, EventArgs e)
         {
+            var clickedButton = sender as Button;
+            if (clickedButton.Text.Contains("Poprzedni"))
+            {
+                if (SelectedImageIndex > 0)
+                {
+                    SelectedImageIndex -= 1;
+                    Image slectedImg = LoadedImages[SelectedImageIndex];
+                    pictureBox1.Image = slectedImg;
 
+                    SelectTheClickedItem(listView1, SelectedImageIndex);
+                }
+            }
+            else
+            {
+                if (SelectedImageIndex < (LoadedImages.Count -1 ))
+                {
+                    SelectedImageIndex += 1;
+                    Image slectedImg = LoadedImages[SelectedImageIndex];
+                    pictureBox1.Image = slectedImg;
+
+                    SelectTheClickedItem(listView1, SelectedImageIndex);
+                }
+            }
+        }
+
+        private void SelectTheClickedItem(ListView list, int index)
+        {
+            for(int item = 0; item < list.Items.Count; item++)
+            {
+                if (item == index)
+                {
+                    list.Items[item].Selected = true;
+                }
+                else
+                {
+                    list.Items[item].Selected = false;
+                }
+            }           
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                var selectedIndex = listView1.SelectedIndices[0];
+                Image slectedImg = LoadedImages[selectedIndex];
+                pictureBox1.Image = slectedImg;
+                SelectedImageIndex = selectedIndex;
+            }
+        }
+
+       MaterialSkinManager TManager = MaterialSkinManager.Instance;
+
+        private void materialSwitch1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (materialSwitch1.Checked)
+            {
+                TManager.Theme = MaterialSkinManager.Themes.DARK;
+                
+            }
+            else
+            {
+                TManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            }
+        }
+
+        private void materialTextBox21_Click(object sender, EventArgs e)
+        {
+          //show image file name in textbox
+          string fileName = SelectedImageIndex.ToString();
+          materialTextBox21.Text = fileName;
         }
     }
 }
